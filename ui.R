4ef8024b624a0c644ec3b548eac8298c1e48d74f
anupb08 <anupbera@drishti-soft.com>
2015-09-27 13:22:23 +0530

    modified

--- ui.R
+++ ui.R
@@ -3,7 +3,7 @@ shinyUI(pageWithSidebar(
    headerPanel("Prediction of Diamond Price"),
       sidebarPanel(
       selectInput('Shape', 'Select Shape:', c('Heart' ,'Round', 'Princess', 'Cushion','Pear','Marquise', 'Emerald', 'Radiant', 'Oval', 'Asscher')),
-      numericInput('Carat', 'Enter the weight in carats', 1.0 , min = .32, max = 4.0, step = 0.01),
+      numericInput('Carat', 'Enter the weight in carats (0.32 to 4.0)', 1.0 , min = .32, max = 4.0, step = 0.01),
       selectInput('Cut', 'Select Cut:', c('Good' ,'Ideal', 'V.Good')),
       selectInput('Color', 'Select Color:', c('D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L')),
       selectInput('Clarity', 'Select Clarity:', c('I1', 'I2', 'IF', 'SI1', 'SI2', 'VS1', 'VS2', 'VVS1', 'VVS2')),
